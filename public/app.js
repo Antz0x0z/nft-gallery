2f89d681d889663610af7afcee4b108d18559d15
Kern Anton <aakern@beeline.ru>
2022-11-01 19:39:51 +0700

    localhost version

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
+app.listen(3001, () => {
+  console.log("Application started and Listening on port 3001");
 });
 
 // serve your css as static
--- public/index.js
+++ public/index.js
@@ -1,72 +1,24 @@
-import GameOfTheMindABI from "./GameOfTheMind.js"
+import abi from "./ContractOnCEMNetwork.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const web3 = new Web3("https://cemchain.com/")
+  // const web3 = new Web3("https://cemchain.com/")
+  const web3 = new Web3("http://localhost:8545/")
   web3.eth.getBlockNumber().then((result) => {
     console.log("Latest Ethereum Block is ", result);
   });
 
   document.getElementById("load_button").addEventListener("click", async () => {
-    //const walletAddress = document.getElementById("wallet_address").value
     const walletAddress = document.getElementById("wallet_address").value
-    const contract = new web3.eth.Contract(GameOfTheMindABI, walletAddress)
+    const contract = new web3.eth.Contract(abi, walletAddress)
     contract.defaultAccount = walletAddress
 
     document.getElementById("nfts").innerHTML = ""
 
-    if (walletAddress == "0xb826256e6a6ffdd79aa6a7707a0a916f8ce41950") {
-      for (let i = 1; i < 4; i++) {  //0xb826256e6a6ffdd79aa6a7707a0a916f8ce41950 (Gandalf)
-        let tokenMetadataURI = await contract.methods.uri(i).call()
-        let repl = tokenMetadataURI.replace('{id}', i) // (rand images)
-
-        if (repl.startsWith("ipfs://")) { // (rand images)
-          repl = `https://ipfs.io/ipfs/${repl.split("ipfs://")[1]}`
-        }
-        const tokenMetadata = await fetch(repl).then((response) => response.json())
-
-        const tokenElement = document.getElementById("nft_template").content.cloneNode(true)
-        tokenElement.querySelector("h1").innerText = tokenMetadata["name"]
-
-        const elem = tokenMetadata["image"]
-        if (elem.startsWith("ipfs://")) {
-          tokenElement.querySelector("img").src = `https://ipfs.io/ipfs/${elem.split("ipfs://")[1]}`
-          tokenElement.querySelector("a").href = `https://ipfs.io/ipfs/${elem.split("ipfs://")[1]}`
-        }
-        tokenElement.querySelector("img").alt = tokenMetadata["description"]
-        tokenElement.querySelector("h2").innerText = tokenMetadata["description"]
-
-        document.getElementById("nfts").append(tokenElement)
-      }
-    }
-
-    if (walletAddress == "0x992A2B4C10f5d607c68e686Cd46aA7cdD018fa8E") {
-      for (let i = 100; i < 160; i++) {  //0x992A2B4C10f5d607c68e686Cd46aA7cdD018fa8E (rand images)
-        let tokenMetadataURI = await contract.methods.uri(i).call()
-        let repl = tokenMetadataURI.replace('{id}', i) // (rand images)
-
-        if (repl.startsWith("ipfs://")) { // (rand images)
-          repl = `https://ipfs.io/ipfs/${repl.split("ipfs://")[1]}`
-        }
-        const tokenMetadata = await fetch(repl).then((response) => response.json())
-
-        const tokenElement = document.getElementById("nft_template").content.cloneNode(true)
-        tokenElement.querySelector("h1").innerText = tokenMetadata["name"]
-
-        const elem = tokenMetadata["image"]
-        if (elem.startsWith("ipfs://")) {
-          tokenElement.querySelector("img").src = `https://ipfs.io/ipfs/${elem.split("ipfs://")[1]}`
-          tokenElement.querySelector("a").href = `https://ipfs.io/ipfs/${elem.split("ipfs://")[1]}`
-        }
-        tokenElement.querySelector("img").alt = tokenMetadata["description"]
-        tokenElement.querySelector("h2").innerText = "(＠´ー`)ﾉﾞ＼(＾▽＾)／"
-
-        document.getElementById("nfts").append(tokenElement)
-      }
-    }
-
-    if (walletAddress == "0x790e36496c3bb299BA849253b6e930F546451699") {
-      for (let i = 1; i < 20; i++) {     //0x790e36496c3bb299BA849253b6e930F546451699 (BAYC)
-        let tokenMetadataURI = await contract.methods.uri(i).call()
+	  let count = await contract.methods.listedItemsCount().call();
+    console.log("listedItemsCount is ", count);
+      for (let i = 1; i <= count; i++) {
+        let tokenMetadataURI = await contract.methods.getURI(i).call();
+        console.log("tokenMetadataURI is ", tokenMetadataURI);
 
         if (tokenMetadataURI.startsWith("ipfs://")) { // (BAYC)
           tokenMetadataURI = `https://ipfs.io/ipfs/${tokenMetadataURI.split("ipfs://")[1]}`
@@ -79,13 +31,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const elem = tokenMetadata["image"]
         if (elem.startsWith("ipfs://")) {
           tokenElement.querySelector("img").src = `https://ipfs.io/ipfs/${elem.split("ipfs://")[1]}`
-          tokenElement.querySelector("a").href = `https://ipfs.io/ipfs/${elem.split("ipfs://")[1]}`
+          tokenElement.querySelector("a").href = tokenMetadata["external_link"]
         }
         tokenElement.querySelector("img").alt = tokenMetadata["description"]
         tokenElement.querySelector("h2").innerText = tokenMetadata["description"]
 
         document.getElementById("nfts").append(tokenElement)
       }
-    }
   })
 })
